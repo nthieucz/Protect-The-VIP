f86ac570dfc8c0969052cbcd5a60d8cb0adc0238
BioFrozt <58526137+BioFrozt@users.noreply.github.com>
2020-10-02 20:42:57 +0100

    sniper + proj and destructible scripts

---
+++ Protect The VIP/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+
+    public Vector3 direction;
+    private float lifeSpan = 0;
+    public float speed;
+   
+    void Update()
+    {
+        gameObject.transform.Translate(direction * Time.deltaTime * speed);
+        lifeSpan += Time.deltaTime;
+        if (lifeSpan > 2)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Destroy(gameObject);
+    }
+}
--- Protect The VIP/Assets/Scripts/SniperScript.cs
+++ Protect The VIP/Assets/Scripts/SniperScript.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class SniperScript : MonoBehaviour
 {
-    public GameObject sniper;
+    public GameObject projectilePrefab;
+    public GameObject destructiblePrefab;
+
+    Vector3 currentPos;
+
+    private void Start()
+    {
+        currentPos = new Vector3(0, 0, 0);
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            this.shoot(currentPos, worldPosition);
         }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            GameObject newDestructible = Instantiate(destructiblePrefab);
+            newDestructible.transform.position = worldPosition;
+        }
+    }
+
+    void shoot(Vector3 initial, Vector3 final)
+    {
+        Vector3 direction = final - initial;
+
+        GameObject projectile = Instantiate(projectilePrefab);
+        projectile.transform.position = initial;
+        projectile.GetComponent<ProjectileScript>().direction = direction.normalized; ;
     }
 }
