88d7759f651ff7ce3b61c29b0d4c2540967ffdd4
BioFrozt <58526137+BioFrozt@users.noreply.github.com>
2020-10-02 19:07:18 +0100

    sniper

---
+++ Protect The VIP/Assets/Scripts/SniperScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperScript : MonoBehaviour
+{
+    public GameObject sniper;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
