7e27522f4f72ae71a263b09d1aa02e6049b1501f
Nguyen Thanh Hieu <2401707n@student.gla.ac.uk>
2020-10-03 21:33:10 +0200

    Brick Rope

--- Protect The VIP/Assets/Scripts/Bomb.cs
+++ Protect The VIP/Assets/Scripts/Bomb.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public GameObject explosion;
+    public GameObject explosionEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class Bomb : MonoBehaviour
         if (Input.GetButton("Jump"))
         {
             Instantiate(explosion, transform.position, Quaternion.identity);
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
             gameObject.SetActive(false);
         }
     }
