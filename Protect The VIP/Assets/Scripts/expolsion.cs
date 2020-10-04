b9ee7bce873e938793b07fa8a71e49243bdc758f
Nguyen Thanh Hieu <2401707n@student.gla.ac.uk>
2020-10-04 13:23:49 +0200

    SDADA

--- Protect The VIP/Assets/Scripts/expolsion.cs
+++ Protect The VIP/Assets/Scripts/expolsion.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class expolsion : MonoBehaviour
 {
+    public AudioClip audio;
+    private SFXManager sfxManager;
     private float timer;
     public float timerStart;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        sfxManager = GameObject.Find("SFX Manager").GetComponent<SFXManager>();
+        sfxManager.playAudio(audio);
     }
 
     // Update is called once per frame
@@ -18,6 +22,7 @@ public class expolsion : MonoBehaviour
         timer += Time.deltaTime;
         if (timer > timerStart)
         {
+            
             Destroy(gameObject);
         }
     }
